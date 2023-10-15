31936c762540a3e2e87274934a34717e7bb5bfc4
ChafiaB <chafiaberri@gmail.com>
2023-10-15 15:14:36 -0400

    committed

---
+++ src/git/ABC.java
@@ -0,0 +1,9 @@
+package git;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/git/Git.java
@@ -0,0 +1,17 @@
+package git;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+
+	}
+	
+	public void test() {
+		
+		
+		
+	}
+
+}
